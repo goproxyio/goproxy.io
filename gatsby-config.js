0cd305d3d29b530b0433435220a37373698183b9
Cao Junchao <alexchao1990@gmail.com>
2020-02-21 12:08:00 +0800

    Fix the missing gatsby-plugin-styled-components config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-lodash',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
